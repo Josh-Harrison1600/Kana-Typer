3de7026fec25c208a3a509381275755acfee0820
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2025-03-06 16:18:21 -0400

    adjusted quiz UI

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,6 +7,8 @@ import RefreshIcon from "@mui/icons-material/Refresh";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Tooltip from '@mui/material/Tooltip';
 import { romanjiMap } from './kana';
+import correct from '../assets/audio/correct.mp3';
+import incorrect from '../assets/audio/incorrect.mp3';
 
 const Game = () => {
     const navigate = useNavigate();
@@ -16,57 +18,97 @@ const Game = () => {
     //shuffle characters randomly
     const shuffleArray = (array: string[]) => [...array].sort(() => Math.random() - 0.5);
     const [shuffledChars, setShuffledChars] = useState<string[]>(shuffleArray(selectedChars));
+    const [retryQueue, setRetryQueue] = useState<string[]>([]);
+
 
     const [currentIndex, setCurrentIndex] = useState(0);
     const [userInput, setUserInput] = useState('');
     const [message, setMessage] = useState('');
     const [correctCount, setCorrectCount] = useState(1);
     const [incorrectCount, setIncorrectCount] = useState(0);
+    const [showCorrect, setShowCorrect] = useState(false);
+    const [pendingRetry, setPendingRetry] = useState<string | null>(null); 
+    const [quizCompleted, setQuizCompleted] = useState(false);
 
     //restart game if selectedChars changes
     useEffect(() => {
         setShuffledChars(shuffleArray(selectedChars));
+        setRetryQueue([]);
         setCurrentIndex(0);
+        setQuizCompleted(false);
     }, [selectedChars]);
 
     const checkAnswer = () => {
+        if(quizCompleted) return;
+
         const currentKana = shuffledChars[currentIndex];
         const correctAnswer = romanjiMap[currentKana];
 
         if (userInput.trim().toLowerCase() === correctAnswer) {
-            setMessage('correct answer');
+            setMessage('Correct!');
+            new Audio(correct).play();
             setCorrectCount(prevCount => prevCount + 1);
+            setShowCorrect(false);
+
             setTimeout(() => {
                 setUserInput('');
                 setMessage('');
-                if (currentIndex + 1 < shuffledChars.length) {
-                    setCurrentIndex(currentIndex + 1);
-                } else {
-                    const total = correctCount + incorrectCount + 1;
-                    const accuracy = total === 0 ? 0 : ((correctCount + 1) / total) * 100;
-                    setMessage(`Finished quiz! Accuracy: ${accuracy.toFixed(2)}%`);
-                }
+                moveToNext();
             }, 500);
-        } else {
-            setMessage('Incorrect answer, try again');
-            setIncorrectCount(prevCount => prevCount + 1);
+        }else{
+            setMessage(`Incorrect. Correct Answer: ${correctAnswer}`);
+            new Audio(incorrect).play();
+            setIncorrectCount(prev => prev + 1);
+            setShowCorrect(true);
+            setPendingRetry(currentKana);
+        }
+    };
+
+
+    //move to next question when user guesses wrong
+    const moveToNext = () => {
+        setUserInput('');
+        setMessage('');
+        setShowCorrect(false);
+        if(pendingRetry){
+            //add incorrect answer into random position
+            const newShuffledlist = [...shuffledChars];
+            const insertIndex = Math.floor(Math.random() * (shuffledChars.length - currentIndex - 1))
+            newShuffledlist.splice(insertIndex, 0, pendingRetry);
+            setShuffledChars(newShuffledlist);
+            setPendingRetry(null);
+        }
+
+        if(currentIndex + 1 < shuffledChars.length){
+            setCurrentIndex(currentIndex + 1)
+        }else{
+            //all questions are answered
+            const total = correctCount + incorrectCount;
+            const accuracy = total === 0 ? 0 : (correctCount / total) * 100;
+            setMessage(`Finished Quiz with ${accuracy.toFixed(2)}% Accuracy!`)
+            setQuizCompleted(true);
         }
     };
 
     const restartGame = () => {
         setShuffledChars(shuffleArray(selectedChars));
+        setRetryQueue([]);
         setCurrentIndex(0);
         setCorrectCount(0);
         setIncorrectCount(0);
         setMessage('');
         setUserInput('');
+        setShowCorrect(false);
+        setPendingRetry(null);
+        setQuizCompleted(false);
     };
 
     return (
         <div className='min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white p-6'>
-            {shuffledChars.length > 0 && currentIndex < shuffledChars.length ? (
+            {!quizCompleted ? (
                 <>
-                    <h1 className='text-4xl font-bold mb-4'>{shuffledChars[currentIndex]}</h1>
+                    {/* Show kana/question when quiz is active */}
+                    <h1 className='text-4xl font-bold mb-4 mt-12'>{shuffledChars[currentIndex]}</h1>
                     
                     {/* Input Field with Arrow */}
                     <TextField
@@ -77,7 +119,7 @@ const Game = () => {
                         onKeyDown={(e) => {
                             if (e.key === "Enter") {
                                 e.preventDefault();
-                                checkAnswer();
+                                showCorrect ? moveToNext() : checkAnswer();
                             }
                         }}
                         className='mb-4 bg-white rounded-lg'
@@ -97,7 +139,7 @@ const Game = () => {
                         slotProps={{
                             input: {
                                 endAdornment: (
-                                    <IconButton onClick={checkAnswer}>
+                                    <IconButton onClick={showCorrect ? moveToNext : checkAnswer}>
                                         <ArrowForwardIcon sx={{ color: "black" }} />
                                     </IconButton>
                                 ),
@@ -110,27 +152,28 @@ const Game = () => {
                     />
 
                     {/* Message Display */}
-                    <p className='mt-4 text-lg'>{message}</p>
-
-                    {/* Bottom Navigation Icons */}
-                    <div className="flex flex-row gap-6 mt-6">
-                        <Tooltip title="Return to Menu" arrow>
-                            <IconButton onClick={() => navigate("/")} sx={{ color: "red" }}>
-                                <ArrowBackIcon fontSize="large" />
-                            </IconButton>
-                        </Tooltip>
-                        <Tooltip title="Restart Quiz" arrow>
-                            <IconButton onClick={restartGame} sx={{ color: "red" }}>
-                                <RefreshIcon fontSize="large" />
-                            </IconButton>
-                        </Tooltip>
-                    </div>
+                    <p className='mt-4 text-lg min-h-[32px]'>{message}</p>
                 </>
             ) : (
-                <IconButton onClick={() => navigate("/")} sx={{ color: "magenta" }}>
-                    <ArrowBackIcon fontSize="large" />
-                </IconButton>
+                <>
+                    {/* Display Final Message when Quiz is Done */}
+                    <h1 className='text-3xl font-bold text-center'>{message}</h1>
+                </>
             )}
+
+            {/* Bottom Navigation Icons (Always Show) */}
+            <div className="flex flex-row gap-6 mt-6">
+                <Tooltip title="Return to Menu" arrow>
+                    <IconButton onClick={() => navigate("/")} sx={{ color: "red" }}>
+                        <ArrowBackIcon fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Restart Quiz" arrow>
+                    <IconButton onClick={restartGame} sx={{ color: "red" }}>
+                        <RefreshIcon fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+            </div>
         </div>
     );
 };
